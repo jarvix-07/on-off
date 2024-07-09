6e72c9611da383f46032268d0fe0895a3f8cc9e3
jarvix-07 <prithivrajme42@gmail.com>
2024-07-09 16:24:34 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,14 @@
+var bulb=0;
+
+function findbulb()
+{
+    if(bulb==0)
+    {
+        document.getElementById("image").src = "on.jpg"
+        bulb=1;
+    }
+    else{
+        document.getElementById("image").src = "off.jpg"
+        bulb=0;
+    }
+}
